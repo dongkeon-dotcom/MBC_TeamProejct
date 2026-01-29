5c7b5bd2b469e5e8e35c259936756ecb96eac063
dongkeon <suntan1234@naver.com>
2026-01-29 10:42:03 +0900

    DB 경로수정, ProductDao, Service 작성

---
+++ src/main/java/com/mbcTeam/controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.mbcTeam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/shop")
+@Controller
+public class ProductController {
+	
+	
+
+}
