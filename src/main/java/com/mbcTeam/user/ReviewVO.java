299efa9f246aa5a331bff67319551938658f829b
dongkeon <suntan1234@naver.com>
2026-02-11 10:24:53 +0900

    Orders, Reviews Table 컬럼 추가에 따른 vo 값 추가

--- src/main/java/com/mbcTeam/user/ReviewVO.java
+++ src/main/java/com/mbcTeam/user/ReviewVO.java
@@ -8,10 +8,11 @@ public class ReviewVO {
 	private long productIdx;			// 제품번호
 	private long userIdx;				// 사용자번호
 	private String userName;			// 사용자이름
-	private String review;			// 후기내용
-	private int rating;			// 별점
-	private Boolean isHide= false;			// 후기숨김
-	private String regDate;		// 후기등록일
+	private String review;				// 후기내용
+	private int rating;					// 별점
+	private Boolean isHide= false;		// 후기숨김
+	private String regDate;				// 후기등록일
+	private String Item_Idx;			// 구매한 제품 주문정보 idx
 	
 	
 	//   orderdetailList에서 후기작성시 각각으 ㅣ상품에 대한 후기작성을 위해서 상품옵션인 컬러와 사이즈를 가져옴 
